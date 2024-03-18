d5a00581e06705a2a2b8f4ddf64986e299e7213e
Alberto <a.morcillo@sapalomera.cat>
2024-03-18 18:55:46 +0100

    envia el correo para cmabiar de contraseña

--- app/Models/Articulo.php
+++ app/Models/Articulo.php
@@ -9,19 +9,15 @@ class Articulo extends Model
 {
     use HasFactory;
 
-    // Aquí se especifican los campos que pueden ser asignados de forma masiva.
     protected $fillable = [
         'titulo',
         'contenido',
-        'user_id', // Asegúrate de que este campo coincide con tu columna en la base de datos.
+        'user_id', 
     ];
 
-    /**
-     * Obtener el usuario que creó el artículo.
-     */
     public function user()
     {
-        // Asegúrate de que el namespace del modelo User es correcto y que exista la relación en la base de datos.
+     
         return $this->belongsTo(User::class);
     }
 
